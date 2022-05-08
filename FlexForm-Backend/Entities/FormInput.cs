6bf8c162bf13498c83e4b25c36c8f214387dadf4
tudchalee <tudchapan.lee@mail.kmutt.ac.th>
2022-05-08 12:09:19 +0700

    feat: delete login file and update label in forminput

--- FlexForm-Backend/Entities/FormInput.cs
+++ FlexForm-Backend/Entities/FormInput.cs
@@ -36,6 +36,35 @@ public class ComponentFormInput
     [BsonElement("ComponentId")] 
     public string ComponentId { get; set; } 
     
+    [BsonElement("ComponentLabel")] 
+    public List<string> ComponentLabel { get; set; } 
+    
     [BsonElement("ComponentValue")] 
     public List<string> ComponentValue { get; set; } 
 }
+
+public class UserInfo
+{
+    public string UserName { get; set; }
+
+    public int Age { get; set; }
+}
+
+public class DemoResponse<T>  
+{  
+    public int Code { get; set; }  
+  
+    public string Msg { get; set; }  
+  
+    public T Data { get; set; }  
+  
+    public static DemoResponse<T> GetResult(int code, string msg, T data = default(T))  
+    {  
+        return new DemoResponse<T>  
+        {  
+            Code = code,  
+            Msg = msg,  
+            Data = data  
+        };  
+    }  
+}  
