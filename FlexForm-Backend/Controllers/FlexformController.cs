24501bd3fd331706650f2a6a4407d8428105dbb2
tudchalee <tudchapan.lee@mail.kmutt.ac.th>
2022-05-09 11:36:05 +0700

    feat: delete by mongoid

--- FlexForm-Backend/Controllers/FlexformController.cs
+++ FlexForm-Backend/Controllers/FlexformController.cs
@@ -2,7 +2,6 @@
 using FlexForm_Backend.Entities;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
-using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace FlexForm_Backend.Controllers;
 
@@ -89,14 +88,14 @@ public class FormInputController : ControllerBase
     }
     
         
-    // GET: api/Flexform/AllForm
+    // GET: api/FlexInput/AllForm
     [HttpGet("AllFormInput")]
     public ActionResult<List<FormInput>> GetAllFormInput()
     {
         return forminputService.GetAllFormInput();
     }
 
-    // GET api/Flexform/FormInput/{id}
+    // GET api/FormInput/FormInput/{id}
     [HttpGet("FormInput/{id}")]
     public ActionResult<List<FormInput>> GetByIdFormInput(string id)
     {
@@ -110,63 +109,157 @@ public class FormInputController : ControllerBase
         return form;
     }
     
-    // POST api/Flexform/CreateForm
+    // GET api/FormInput/Mongo/{id}
+    [HttpGet("Mongo/{id}")]
+    public ActionResult<FormInput> GetByMongoIdFormInput(string id)
+    {
+        var form = forminputService.GetByMongoIdFormInput(id);
+    
+        if (form == null)
+        {
+            return NotFound($"Form with Id = {id} not found");
+        }
+    
+        return form;
+    }
+    
+    // POST api/FormInput/CreateForm
     [HttpPost("CreateFormInput")]
     public ActionResult<FormInput> Post([FromBody] FormInput form)
     {
         var savedForm = forminputService.CreateFormInput(form);
         return savedForm;
     }
+    
+    //DELETE api/FormInput/Delete/Mongo/{id}
+    [HttpDelete("Delete/Mongo/{id}")]
+    public ActionResult Delete(string id)
+    {
+        var form = forminputService.GetByMongoIdFormInput(id);
+        Console.WriteLine("Hello" + form);
+        if (form == null)
+        {
+            return NotFound($"Form with Id = {id} not found");
+        }
+        
+        forminputService.RemoveByMongoId(form.Id);
+    
+        return Ok($"Form with Id = {id} deleted");
+    }
 }
+
 
 [Route("api/[controller]")]
 [ApiController]
 public class ImportExportController : ControllerBase
- {
-  private readonly IHostingEnvironment _hostingEnvironment;
-
-  public ImportExportController(IHostingEnvironment hostingEnvironment)
-  {
-   _hostingEnvironment = hostingEnvironment;
-  }
-  [HttpGet("Import")]
-  public string Import()
+{
+    [HttpGet("Import")]
+    public string Import()
     {
-      string sWebRootFolder = @"D:\";
-     string sFileName = @"UserList-25650506145952547.xlsx";
-     FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
-     try
-     {
-      using (ExcelPackage package = new ExcelPackage(file))
-      {
-       StringBuilder sb = new StringBuilder();
-       ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
-       int rowCount = worksheet.Dimension.Rows;
-       int ColCount = worksheet.Dimension.Columns;
-       bool bHeaderRow = true;
-       for (int row = 1; row <= rowCount; row++)
-       {
-        for (int col = 1; col <= ColCount; col++)
+        string sWebRootFolder = @"D:\";
+        string sFileName = @"UserList-25650506145952547.xlsx";
+        FileInfo file = new FileInfo(Path.Combine(sWebRootFolder, sFileName));
+        try
         {
-         if (bHeaderRow)
-         {
-          sb.Append(worksheet.Cells[row, col].Value.ToString() + "\t");
-         }
-         else
-         {
-          sb.Append(worksheet.Cells[row, col].Value.ToString() + "\t");
-         }
+            using (ExcelPackage package = new ExcelPackage(file))
+            {
+                StringBuilder sb = new StringBuilder();
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                int rowCount = worksheet.Dimension.Rows;
+                int ColCount = worksheet.Dimension.Columns;
+                bool bHeaderRow = true;
+                for (int row = 1; row <= rowCount; row++)
+                {
+                    for (int col = 1; col <= ColCount; col++)
+                    {
+                        if (bHeaderRow)
+                        {
+                            sb.Append(worksheet.Cells[row, col].Value.ToString() + "\t");
+                        }
+                        else
+                        {
+                            sb.Append(worksheet.Cells[row, col].Value.ToString() + "\t");
+                        }
+                    }
+
+                    sb.Append(Environment.NewLine);
+                }
+
+                return sb.ToString();
+            }
         }
-        sb.Append(Environment.NewLine);
-       }
-       return sb.ToString();
-      }
-     }
-     catch (Exception ex)
-     {
-      return "Some error occured while importing." + ex.Message;
-     }
+        catch (Exception ex)
+        {
+            return "Some error occured while importing." + ex.Message;
+        }
     }
- }
-    
+
+    public List<ComponentFormInput> itemlist;
+        
+    [HttpGet("exportv2")]
+    public async Task<IActionResult> ExportV2(CancellationToken cancellationToken)
+    {
+        // query data from database  
+        await Task.Yield();
+        var list = new List<UserInfo>()
+        {
+            new UserInfo {UserName = "catcher", Age = 18},
+            new UserInfo {UserName = "james", Age = 20},
+        };
+        
+
+        var stream = new MemoryStream();
+
+            using (var package = new ExcelPackage(stream))
+            {
+                var workSheet = package.Workbook.Worksheets.Add("Sheet1");
+                for (int i = 0; i < itemlist.Count; i++)
+                {
+                    var item = itemlist[i];
+                    workSheet.Cells[i + 2, 1].Value = item.ComponentValue;
+                }
+                workSheet.Cells.LoadFromCollection(list, true);
+                package.Save();
+            }
+
+            stream.Position = 0;
+            string excelName = $"UserList-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.xlsx";
+
+            //return File(stream, "application/octet-stream", excelName);  
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
+        }
+
+        [HttpGet("export")]
+        public async Task<DemoResponse<string>> Export(CancellationToken cancellationToken)
+        {
+            string folder = @"D:\Download";
+            string excelName = $"UserList-{DateTime.Now.ToString("yyyyMMddHHmmssfff")}.xlsx";
+            string downloadUrl = string.Format("{0}://{1}/{2}", Request.Scheme, Request.Host, excelName);
+            FileInfo file = new FileInfo(Path.Combine(folder, excelName));
+            if (file.Exists)
+            {
+                file.Delete();
+                file = new FileInfo(Path.Combine(folder, excelName));
+            }
+
+            // query data from database  
+            await Task.Yield();
+
+            var list = new List<UserInfo>()
+            {
+                new UserInfo {UserName = "catcher", Age = 18},
+                new UserInfo {UserName = "james", Age = 20},
+            };
+
+            using (var package = new ExcelPackage(file))
+            {
+                var workSheet = package.Workbook.Worksheets.Add("Sheet1");
+                workSheet.Cells.LoadFromCollection(list, true);
+                package.Save();
+            }
+
+            return DemoResponse<string>.GetResult(0, "OK", downloadUrl);
+        }
+    }
+
 
--- FlexForm-Backend/Services/FormInputServices.cs
+++ FlexForm-Backend/Services/FormInputServices.cs
@@ -1,15 +1,18 @@
 ﻿using FlexForm_Backend.Entities;
+using FlexForm_Backend.Models;
 using FlexForm_Backend.Helper;
 using Microsoft.EntityFrameworkCore.Metadata;
 using MongoDB.Driver;
 
 namespace FlexForm_Backend.Services;
-
 public interface IFormInputService
 {
     List<FormInput> GetAllFormInput();
     List<FormInput> GetByIdFormInput(string id);
+    FormInput GetByMongoIdFormInput(string id);
     FormInput CreateFormInput(FormInput form);
+    void RemoveByMongoId(string id);
+    // ImportExport GetExport();
 }
 public class FormInputServices : IFormInputService
 {
@@ -35,4 +38,19 @@ public class FormInputServices : IFormInputService
     {
         return _formInputs.Find(formInput => formInput.FormId == id).ToList();
     }
+    
+    public FormInput GetByMongoIdFormInput(string id)
+    {
+        return _formInputs.Find(formInput => formInput.Id == id).FirstOrDefault();
+    }
+    
+    public void RemoveByMongoId(string id)
+    {
+        _formInputs.DeleteOne(formInput => formInput.Id == id);
+    }
+
+    // public ImportExport GetExport()
+    // {
+    //     return WebRootPath;
+    // }
 }
